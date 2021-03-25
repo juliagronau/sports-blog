eac062a7f7b69ebe7d2ac89cc15c0968ffb114db
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-03-25 17:47:42 +0100

    Solve container layout, improve Footer, add Pagination

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <Container>
-        <App />
-      </Container>
+      <App />
    </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
