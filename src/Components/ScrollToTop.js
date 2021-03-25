294aa4a18aedf431d60c04280d76dbb50f05cfd3
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-03-25 20:28:40 +0100

    No scroll when open article

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -5,7 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if(pathname === '/') window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
