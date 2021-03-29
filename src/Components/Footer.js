8a5ad4213c9e897257a359f05a77de1a1437bd27
Julia <77528734+juliagronau@users.noreply.github.com>
2021-03-29 18:38:56 +0200

    search only works after second click

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,89 +4,115 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <>
-    <div className="footer">
-      <div className="overlayFooter"></div>
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-12 col-lg-6 mt-3">
-						<h3>About us</h3>
-						<div className="row">
-						  <div className="col-1">
-                <FontAwesomeIcon icon={['fa', 'map-pin']} fixedWidth/>
+      <div className="footer">
+        <div className="overlayFooter"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-lg-6 mt-3">
+              <h3>About us</h3>
+              <div className="row">
+                <div className="col-1">
+                  <FontAwesomeIcon icon={["fa", "map-pin"]} fixedWidth />
+                </div>
+                <div className="col">
+                  Halle & Berlin, old city and new capital of germany
+                </div>
               </div>
-              <div className="col">
-                Halle & Berlin, old city and new capital of germany
+              <div className="row">
+                <div className="col-1">
+                  <FontAwesomeIcon icon={["fa", "headset"]} fixedWidth />
+                </div>
+                <div className="col">
+                  Call us via <a href="https://discord.com/">Discord</a>
+                </div>
               </div>
+              <div className="row">
+                <div className="col-1">
+                  <FontAwesomeIcon icon={["fa", "envelope-open"]} fixedWidth />
+                </div>
+                <div className="col">thesportsblog@posteo.de</div>
+              </div>
+              <hr className="hr" />
+              <h3>Follow us</h3>
+              <div className="row">
+                <div className="col-1 pointer">
+                  <FontAwesomeIcon icon={["fab", "facebook"]} />
+                </div>
+                <div className="col-1 pointer">
+                  <FontAwesomeIcon icon={["fab", "instagram"]} />
+                </div>
+                <div className="col-1 pointer">
+                  <FontAwesomeIcon icon={["fab", "twitter"]} />
+                </div>
+                <div className="col-1 pointer">
+                  <FontAwesomeIcon icon={["fab", "github"]} />
+                </div>
+                <div className="col-1 pointer">
+                  <FontAwesomeIcon icon={["fab", "react"]} />
+                </div>
+              </div>
+              <hr className="hr" />
+              <p className="copyright">
+                ©2021 Sports Blog - The Blog about Sports in Life. All Rights
+                reserved.
+              </p>
+              <div className="row"></div>
             </div>
-						<div className="row">
-              <div className="col-1">
-                <FontAwesomeIcon icon={['fa', 'headset']} fixedWidth/>
-            	</div>
-							<div className="col">
-								Call us via <a href="https://discord.com/">Discord</a>
-							</div>
-          	</div>
-						<div className="row">
-              <div className="col-1">
-						    <FontAwesomeIcon icon={['fa', 'envelope-open']} fixedWidth/>
-              </div>
-							<div className="col">thesportsblog@posteo.de</div>							
-						</div>
-            <hr className="hr" />
-            <h3>Follow us</h3>
-            <div className="row">
-              <div className="col-1 pointer">
-                <FontAwesomeIcon icon={['fab', 'facebook']} />
-              </div>
-              <div className="col-1 pointer">
-                <FontAwesomeIcon icon={['fab', 'instagram']} />
-              </div>
-              <div className="col-1 pointer">
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
-              </div>
-              <div className="col-1 pointer">
-                <FontAwesomeIcon icon={['fab', 'github']} />
-              </div>
-              <div className="col-1 pointer">
-                <FontAwesomeIcon icon={['fab', 'react']} />
-              </div>
+            <div className="col-sm-12 col-lg-6 mt-3">
+              <form>
+                <h3>Contact us</h3>
+                <div className="mb-3 input-group">
+                  <span className="input-group-text span-custom">
+                    First Name
+                  </span>
+                  <input
+                    type="text"
+                    className="form-control inputfield"
+                    placeholder="..."
+                  />
+                </div>
+                <div className="mb-3 input-group">
+                  <span className="input-group-text span-custom">
+                    Last Name
+                  </span>
+                  <input
+                    type="text"
+                    className="form-control inputfield"
+                    placeholder="..."
+                  />
+                </div>
+                <div className="mb-3 input-group">
+                  <span className="input-group-text span-custom">Email</span>
+                  <input
+                    type="email"
+                    className="form-control inputfield"
+                    placeholder="..."
+                  />
+                  <div id="emailHelp" className="form-text">
+                    Mail is only used to reply
+                  </div>
+                </div>
+                <div className="mb-3 input-group">
+                  <span className="input-group-text span-custom">Message</span>
+                  <textarea
+                    className="form-control inputfield"
+                    aria-label="With textarea"
+                  ></textarea>
+                </div>
+                <button
+                  className="btn bg-light btn-footer"
+                  variant="outline-secondary"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
             </div>
-            <hr className="hr" />
-            <p className="copyright">©2021 Sports Blog - The Blog about Sports in Life. All Rights reserved.</p>
-            <div className="row">
-
-            </div>
-					</div>
-					<div className="col-sm-12 col-lg-6 mt-3">
-						<form>
-							<h3>Contact us</h3>
-								<div className="mb-3 input-group">
-									<span className="input-group-text span-custom">First Name</span>
-									<input type="text" className="form-control inputfield" placeholder="..." />
-								</div>
-							<div className="mb-3 input-group">
-								<span className="input-group-text span-custom">Last Name</span>
-								<input type="text" className="form-control inputfield" placeholder="..." />
-							</div>
-							<div className="mb-3 input-group">
-								<span className="input-group-text span-custom">Email</span>
-								<input type="email" className="form-control inputfield" placeholder="..." />
-								<div id="emailHelp" className="form-text">Mail is only used to reply</div>
-							</div>
-							<div className="mb-3 input-group">
-								<span className="input-group-text span-custom">Message</span>
-								<textarea className="form-control inputfield" aria-label="With textarea"></textarea>
-							</div>
-							<button className="btn bg-light btn-footer" variant="outline-secondary" type="submit">
-								Submit
-							</button>
-						</form>
-        	</div>
-				</div>
-			</div>
-    </div>
-		</>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,29 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 // import fontawesome for React
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import SearchResults from "./SearchResults";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 const Navbar = () => {
-  const [searchTerm, setSearchTerm] = useState();
-  const [posts, setAllPosts] = useState([]);
-  const [hasData, setHasData] = useState(false);
-
-  useEffect(() => {
-    setHasData(true);
-    axios
-      .get("https://blog-project-api-jms.herokuapp.com/posts")
-      .then(response => {
-        setAllPosts(response.data);
-      })
-      .catch(console.error);
-  }, [searchTerm]);
-
-  function onClick() {
-    setSearchTerm(document.querySelector(".searchbar").value);
-  }
-
   return (
     <>
       <div className="container navbar-bg">
@@ -59,38 +38,9 @@ const Navbar = () => {
                 <b>The Blog</b> about Sports in Life
               </div>
             </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarToggler"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarToggler">
-              <form className="d-flex ml-auto">
-                <input
-                  className="form-control searchbar"
-                  type="search"
-                  placeholder="Search your topic"
-                  aria-label="Search"
-                />
-                <span className="input-group-text search-icon-container">
-                  <FontAwesomeIcon
-                    icon={["fa", "search"]}
-                    type="submit"
-                    onClick={onClick}
-                  />
-                </span>
-              </form>
-            </div>
           </div>
         </nav>
       </div>
-      {searchTerm && <SearchResults searchTerm={searchTerm} posts={posts} />}
     </>
   );
 };
