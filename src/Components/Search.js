3d4a35cb352767f8b73c9fde5f5350ce2c16f656
saidakyuz <saidakyuz@gmail.com>
2021-03-29 23:41:59 +0200

    searcbox works pagination not

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,32 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import BlogPostTeaser from "./BlogPostTeaser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactPaginate from "react-paginate";
 import Spinner from "./Spinner";
 
 const Search = ({ posts }) => {
-  const [searchTerm, setSearchTerm] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-  const [displayCards, setDisplayCards] = useState();
+  const [displayCards, setDisplayCards] = useState("");
 
   console.log("Posts", posts);
 
   useEffect(() => {
     setSearchResult(posts);
-    console.log(searchResult);
-  }, [posts]);
+    getFilteredResults();
+  }, [searchTerm]);
 
   const onClick = () => {
-    console.log("onclick works");
-    setSearchTerm(document.querySelector(".searchbar").value);
-    getFilteredResults();
-    console.log(searchTerm);
+    console.log("onclick works", document.querySelector(".searchbar").value);
+    let term = document.querySelector(".searchbar").value;
+    setSearchTerm(term);
+
+    console.log("Kaan", searchTerm);
   };
 
   const getFilteredResults = () => {
     console.log("function invoked");
-    setSearchResult(posts.filter(post => post.posttitle.includes(searchTerm)));
+    setSearchResult(
+      posts.filter((post) =>
+        post.posttitle.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
   };
 
   // Pagination
@@ -38,9 +42,9 @@ const Search = ({ posts }) => {
     // slice() Methode returns part of Arr
     const newArr = searchResult
       .slice(pagesVisited, pagesVisited + cardsPerPage)
-      .map(post => <BlogPostTeaser key={posts.post_id} post={post} />);
+      .map((post) => <BlogPostTeaser key={posts.post_id} post={post} />);
     setDisplayCards(() => newArr);
-  }, [posts, searchResult]);
+  }, [searchResult]);
   // Math.ceil() gives next higher integer that is equal or greater than the given num
   const pageCount = Math.ceil(posts.length / cardsPerPage);
   // ReactPaginate provides argument that I can destructure to {selected}
@@ -56,38 +60,41 @@ const Search = ({ posts }) => {
           type="search"
           placeholder="Search your topic"
           aria-label="Search"
+          onKeyUp={onClick}
         />
         <span className="input-group-text search-icon-container">
           <FontAwesomeIcon
             icon={["fa", "search"]}
             type="submit"
-            onClick={onClick}
+            keyup={onClick}
           />
         </span>
       </form>
       <div className="row">
         {displayCards}
-        <ReactPaginate
-          previousLabel={
-            <FontAwesomeIcon
-              className="previousBtn"
-              icon={["fas", "angle-left"]}
-            />
-          }
-          nextLabel={
-            <FontAwesomeIcon
-              className="nextBtn"
-              icon={["fas", "angle-right"]}
-            />
-          }
-          pageCount={pageCount}
-          onPageChange={changePage}
-          containerClassName={"paginationBtns"}
-          previousLinkClassName={"previousBtn"}
-          nextLinkClassName={"nextBtn"}
-          disabledClassName={"paginationDisabled"}
-          activeClassName={"paginationActive"}
-        />
+        {searchResult.length < 1 || (
+          <ReactPaginate
+            previousLabel={
+              <FontAwesomeIcon
+                className="previousBtn"
+                icon={["fas", "angle-left"]}
+              />
+            }
+            nextLabel={
+              <FontAwesomeIcon
+                className="nextBtn"
+                icon={["fas", "angle-right"]}
+              />
+            }
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBtns"}
+            previousLinkClassName={"previousBtn"}
+            nextLinkClassName={"nextBtn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          />
+        )}
       </div>
     </>
   ) : (
